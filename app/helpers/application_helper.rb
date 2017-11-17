a0d7e19451e3c5fe91ff285c254b9b972d9e93a7
clgoro <clgoro@gmail.com>
2017-11-17 12:31:15 -0500

    commented out tag clouds and created tables for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, alt: user.name)
 	end
-	def tag_cloud(tags, classes)
-    	max = tags.sort_by(&:count).last
-    	tags.each do |tag|
-      	index = tag.count.to_f / max.count * (classes.size - 1)
-      	yield(tag, classes[index.round])
-    end
-  end
+	# def tag_cloud(tags, classes)
+ #    	max = tags.sort_by(&:count).last
+ #    	tags.each do |tag|
+ #      	index = tag.count.to_f / max.count * (classes.size - 1)
+ #      	yield(tag, classes[index.round])
+ #    end
+ #  end
 end
